7e514bfe9b0d7c1333a54ab218346c7f2be28e33
madjo <madjo85@yandex.ru>
2024-02-14 11:13:21 +0300

    Changes reveiw 3. Refactor test class. + variables to constants

--- hw01/src/test/java/TestOfTestServiceImpl.java
+++ hw01/src/test/java/TestOfTestServiceImpl.java
@@ -7,16 +7,15 @@ import ru.otus.hw.domain.Question;
 import ru.otus.hw.service.IOService;
 import ru.otus.hw.service.StreamsIOService;
 import ru.otus.hw.service.TestService;
-import ru.otus.hw.service.TestServiceImpl;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class IOServiceTest {
-    private final String questText = "test1";
-    private final List<Answer> ANSWER_LIST =
+public class TestOfTestServiceImpl {
+    private static final String QUEST_TEXT = "test1";
+    private static final List<Answer> ANSWER_LIST =
             Arrays.asList(new Answer("answer1", true), new Answer("answer2", false));
-    public final String QUESTION_1_STRING = "Question: test1 %n0. answer1 %n1. answer2 %n %n";
+    private static final String QUESTION_1_STRING = "Question: test1 %n0. answer1 %n1. answer2 %n %n";
     private IOService ioServiceMock;
     private Question questionMock;
     private CsvQuestionDao csvQuestionDaoMock;
@@ -24,16 +23,14 @@ public class IOServiceTest {
     @BeforeEach
     public void init() {
         ioServiceMock = Mockito.mock(StreamsIOService.class);
-        questionMock = Mockito.mock(Question.class);
-        Mockito.when(questionMock.text()).thenReturn(questText);
-        Mockito.when(questionMock.answers()).thenReturn(ANSWER_LIST);
+        questionMock = new Question(QUEST_TEXT, ANSWER_LIST);
         csvQuestionDaoMock = Mockito.mock(CsvQuestionDao.class);
         Mockito.when(csvQuestionDaoMock.findAll()).thenReturn(Arrays.asList(questionMock));
     }
 
     @Test
-    public void testIOService() {
-        TestService testService = new TestServiceImpl(ioServiceMock, csvQuestionDaoMock);
+    public void testTestServiceImpl() {
+        TestService testService = new ru.otus.hw.service.TestServiceImpl(ioServiceMock, csvQuestionDaoMock);
         testService.executeTest();
         Mockito.verify(ioServiceMock, Mockito.times(1)).printLine("");
         Mockito.verify(ioServiceMock, Mockito.times(2)).printFormattedLine(Mockito.any());
