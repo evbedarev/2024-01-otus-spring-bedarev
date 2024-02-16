d2f6fe9596db83e1a9afffb57126ff6baf6ca5e0
madjo <madjo85@yandex.ru>
2024-02-16 15:52:00 +0300

    Homework 2 first commit

---
+++ hw02/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -0,0 +1,69 @@
+package ru.otus.hw.dao;
+
+import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class CsvQuestionDao implements QuestionDao {
+    private final TestFileNameProvider fileNameProvider;
+
+    @Override
+    public List<Question> findAll() throws QuestionReadException {
+        List<QuestionDto> quest = getQuestionsFromCsv(fileNameProvider.getTestFileName());
+        List<Question> allQuestList = new ArrayList<>();
+        for (QuestionDto qst : quest) {
+            if (!checkCorrectLine(qst)) {
+                throw new QuestionReadException("Not correct question or too little answers");
+            }
+            allQuestList.add(qst.toDomainObject());
+        }
+        // Использовать CsvToBean
+        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
+        // Использовать QuestionReadException
+        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+        return allQuestList;
+    }
+
+    private List<QuestionDto> getQuestionsFromCsv(String filename) throws QuestionReadException {
+        try (InputStream inputStream = getFileFromResourceAsStream(filename);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            return new CsvToBeanBuilder(reader)
+                    .withType(QuestionDto.class)
+                    .withSkipLines(1)
+                    .withSeparator(';')
+                    .build()
+                    .parse();
+        } catch (IOException e) {
+            throw new QuestionReadException(String.format("Problem read file question or File %s not found",
+                    fileNameProvider.getTestFileName()), e);
+        }
+    }
+
+    private boolean checkCorrectLine(QuestionDto qDto) {
+        return ((qDto.getText().length() > 3) && (qDto.getAnswers().size() > 2));
+    }
+
+    private InputStream getFileFromResourceAsStream(String filename) throws QuestionReadException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(filename);
+        if (inputStream == null) {
+            throw new QuestionReadException(String.format("File %s not found", filename), new RuntimeException());
+        } else {
+            return inputStream;
+        }
+    }
+}
---
+++ hw02/src/test/java/IntegrationTestCsvRead.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ru.otus.hw.config.AppProperties;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.CsvQuestionDao;
+import ru.otus.hw.domain.Question;
+
+import java.util.List;
+
+public class IntegrationTestCsvRead {
+    private static final int Q_LIST_SIZE = 1;
+    private static final int QUESTION_SIZE = 3;
+    private static final String QUESTION = "Is there life on Mars?";
+    private static final String CSV_PATH = "questions.csv";
+    private TestFileNameProvider testFileNameProvider;
+
+    @BeforeEach
+    public void init() {
+        testFileNameProvider = Mockito.mock(AppProperties.class);
+        Mockito.when(testFileNameProvider.getTestFileName()).thenReturn(CSV_PATH);
+    }
+
+    @Test
+    public void testCsvQuestionDao() {
+        CsvQuestionDao csvQuestionDao = new CsvQuestionDao(testFileNameProvider);
+        List<Question> questionList = csvQuestionDao.findAll();
+        Assertions.assertEquals(Q_LIST_SIZE, questionList.size());
+        Assertions.assertTrue(questionList.get(0).text().equals(QUESTION));
+        Assertions.assertEquals(questionList.get(0).answers().size(), QUESTION_SIZE);
+    }
+}
