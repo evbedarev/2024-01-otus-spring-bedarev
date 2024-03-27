ec3550e98ccd9a4c37339022173b813869075502
mj <madjo85@yandex.ru>
2024-03-27 22:37:19 +0300

    first commit Homework 7

---
+++ hw07/src/main/java/ru/otus/hw/converters/CommentsConverter.java
@@ -0,0 +1,21 @@
+package ru.otus.hw.converters;
+
+import org.springframework.stereotype.Component;
+import ru.otus.hw.models.Comment;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class CommentsConverter {
+    public String commentToString(Comment comment) {
+        return "Id: %d, Text: %s BookId: %s".formatted(comment.getId(), comment.getText(),
+                comment.getBookId());
+    }
+
+    public String commentsToString(List<Comment> comments) {
+        return comments == null ? " " : comments.stream().
+                map(c -> "Id: %d, Text: %s".formatted(c.getId(),c.getText())).
+                collect(Collectors.joining(","));
+    }
+}
