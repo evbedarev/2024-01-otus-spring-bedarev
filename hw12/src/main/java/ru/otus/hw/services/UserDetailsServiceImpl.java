012bc90a44f34b3d944639f137c818b9483b3385
mj <madjo85@yandex.ru>
2024-06-05 10:23:24 +0300

    final commit Homework 12

--- hw12/src/main/java/ru/otus/hw/services/UserDetailsServiceImpl.java
+++ hw12/src/main/java/ru/otus/hw/services/UserDetailsServiceImpl.java
@@ -1,15 +1,13 @@
 package ru.otus.hw.services;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.models.User;
 import ru.otus.hw.repositories.UserRepository;
-
-import java.util.Collection;
+import ru.otus.hw.security.OwnUserPrincipal;
 
 @Service
 @RequiredArgsConstructor
@@ -27,47 +25,4 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         }
     }
 
-    class OwnUserPrincipal implements UserDetails {
-
-        private final User user;
-
-        OwnUserPrincipal(User user) {
-            this.user = user;
-        }
-
-        @Override
-        public Collection<? extends GrantedAuthority> getAuthorities() {
-            return null;
-        }
-
-        @Override
-        public String getPassword() {
-            return user.getPassword();
-        }
-
-        @Override
-        public String getUsername() {
-            return user.getUsername();
-        }
-
-        @Override
-        public boolean isAccountNonExpired() {
-            return true;
-        }
-
-        @Override
-        public boolean isAccountNonLocked() {
-            return true;
-        }
-
-        @Override
-        public boolean isCredentialsNonExpired() {
-            return true;
-        }
-
-        @Override
-        public boolean isEnabled() {
-            return true;
-        }
-    }
 }
